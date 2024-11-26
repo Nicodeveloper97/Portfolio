ad5fac9f1db76630ee11537e96b6f680cd494b61
Nicodeveloper97 <nicoiglesiasdeveloper@gmail.com>
2024-11-26 16:30:15 -0300

    se agrega el favicon

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -141,9 +141,9 @@ function Hero({ isDarkMode }: { isDarkMode: boolean }) {
         {isDarkMode ? (
           <Canvas>
             <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-            <ambientLight intensity={0.5} />
+            <ambientLight intensity={1.5} />
             <pointLight position={[10, 10, 10]} />
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+            <Stars radius={150} depth={100} count={5000} factor={5} saturation={0} fade speed={2} />
           </Canvas>
         ) : null}
       </div>
